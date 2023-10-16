074390b1e0d859cfc97e065846b9ae4cba1171f4
jason.burley <jason.burley@sedex.com>
2023-10-16 16:20:06 +0100

    Inventory Service Calls product Service to check if item is in stock. Webflux dependency added to Gradle.

--- order-service/src/main/java/com/jasonpaul/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/jasonpaul/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.jasonpaul.orderservice.service;
 
+import com.jasonpaul.orderservice.dto.InventoryResponse;
 import com.jasonpaul.orderservice.dto.OrderLineItemsDto;
 import com.jasonpaul.orderservice.dto.OrderRequest;
 import com.jasonpaul.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.jasonpaul.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,6 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -31,8 +35,29 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList()
+                .stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
 
+        InventoryResponse[] inventoryResponseArray = callInventoryResponseArray(skuCodes);
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock.");
+        }
+
+    }
+
+    private InventoryResponse[] callInventoryResponseArray(List<String> skuCodes) {
+        return webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
