9a97006e612820d2572a81230333bdf1b22598c7
jason.burley <jason.burley@sedex.com>
2023-10-16 22:17:32 +0100

    API Gateway has been implemented using spring-cloud-starter-gateway. We can now route requests. An additional request was created for the discovery static resources under http (and not 'lb':load balanced) in the application.properties for the gateway.

--- discovery-server/src/main/java/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/discovery_server/DiscoveryServerApplication.java
@@ -1,8 +1,10 @@
-package discovery;
+package discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// Go to http://localhost:8761/ to check discovery.
 
 @SpringBootApplication
 @EnableEurekaServer
